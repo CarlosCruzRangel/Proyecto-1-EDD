898cb83b4e08bb2e04c0d90fa1d1140c83ec3d93
carloscruzrangel <carloscruzrangel@ciencias.unam.mx>
2021-11-06 11:28:40 -0600

    Subida de archivos par empezar
    Clases:
    Jugador, Carta, Mazo, OldMaid, Mano

---
+++ Proyecto1/src/fciencias/edatos/proyecto1/Jugador.java
@@ -0,0 +1,12 @@
+package fciencias.edatos.proyecto1;
+
+/**
+* Solterona, juego de cartas.
+* @author Juan Carlos Zenteno Pompa, Carlos Cruz Rangel
+* @version 1.0 noviembre 2021.
+* @since Estructuras de datos 2022-1. Proyecto 1
+*/
+
+public class Jugador {
+
+}
